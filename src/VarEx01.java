a6edf14ecfa2518194d5eb8c982490193b70db60
Gahyeon Kim”;git config --global user.name Gahyeon <oncehyun@gmail.com>
2023-12-12 14:42:29 +0900

    변수

---
+++ src/VarEx01.java
@@ -0,0 +1,16 @@
+public class VarEx01 {    //모든 코드는 main 메서드 내부에 적는다.
+    public static void main(String[] args) {
+        int n1=1;        //숫자. 21억까지 저장 가능.
+                double n2=1.5; //실수를 저장한다. 8칸을 저장할 수 있다.
+                long big1=2000000000L; //1경을 저장할 수 있다.
+                boolean b1=true; //true 혹은 false를 저장할 수 있다.
+                char c1='나'; //글자 하나를 저장할 수 있다.
+
+                System.out.println(n1);
+                System.out.println(n2);
+                System.out.println(big1);
+                System.out.println(b1);
+                System.out.println(c1);
+        // 자바 프로그램 종료
+    }
+}
