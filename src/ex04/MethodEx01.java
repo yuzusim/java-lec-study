2e5e410ba32345d67d086e9e6981f2860c7c9f3f
yuzusim <yuzusim@gmail.com>
2023-12-22 19:28:50 +0900

    Method

---
+++ src/ex04/MethodEx01.java
@@ -0,0 +1,18 @@
+package ex04;
+
+public class MethodEx01 {
+    static void m1() {
+        System.out.println("m1");
+    }
+
+    static String m2() {
+        System.out.println("m2");
+        return "m2"; // 메서드 종료
+    }
+
+    public static void main(String[] args) {
+        m1();
+        String result = m2();
+        System.out.println("result : " + result);
+    }
+}
