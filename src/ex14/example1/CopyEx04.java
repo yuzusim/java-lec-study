8527bfa709c86899157513022bf4fe5bbf434412
yuzusim <yuzusim@gmail.com>
2024-01-05 12:25:30 +0900

    객체복사 쉬운 방법

--- src/ex14/example1/CopyEx04.java
+++ src/ex14/example1/CopyEx04.java
@@ -5,6 +5,17 @@ class User {
     private String name;
     private String tel;
 
+    public static User copy(User user){
+        return new User(user);
+    }
+
+    // 생성자
+    public User(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.tel = user.getTel();
+    }
+
     // 생성자
     public User(int id, String name, String tel) {
         this.id = id;
@@ -39,12 +50,12 @@ public class CopyEx04 {
     public static void main(String[] args) {
         User u1 = new User(1, "ssar", "0102222");
 
-
         // 1. 통째로 복사
-        User u2 = new User(u1.getId(), u1.getName(),u1.getTel());
+        //User u2 = new User(u1.getId(), u1.getName(),u1.getTel());
+        User u2 = new User(u1); // 사용자 관점에서 쓰기 좋은 코드
 
 
-        // 2. 구분변경 복사
+        // 2. 부분변경 복사
         User u3 = new User(u1.getId(), u1.getName(), "0103333");
 
 
